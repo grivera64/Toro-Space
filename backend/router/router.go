72686f4cc9cc478504f2bee4455f9135584987c3
Giovanni Rivera <rivera.giovanni271@gmail.com>
2024-04-13 00:45:47 -0700

    Add /account/self and refactor session logic

--- backend/router/router.go
+++ backend/router/router.go
@@ -18,6 +18,8 @@ func SetupRoutes(app *fiber.App) {
 	app.Put("/account/:accountID/user/:userID/select", handler.SelectUserHandler)
 	app.Post("/account/:accountID/user/:userID/post", handler.CreatePostHandler)
 
+	app.Get("/user/self", handler.GetCurrentUserHandler)
+
 	// Endpoint: /posts
 	app.Get("/posts", handler.GetPostsHandler)
 
